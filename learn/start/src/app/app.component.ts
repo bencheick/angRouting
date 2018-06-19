81da73491e30cb1cbc9c555b8d6b1e1d07112243
CAMARA <ben-cheick-tidiane.camara@atos.net>
2018-06-19 14:35:09 +0200

    init project

---
+++ learn/start/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { AuthService } from './user/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'pm-app',
+    templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+    pageTitle: string = 'Acme Product Management';
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    logOut(): void {
+        this.authService.logout();
+        console.log('Log out');
+        this.router.navigateByUrl('/welcome');
+    }
+}
---
+++ learn/start/src/app/products/product-edit-info.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { IProduct } from './product';
+
+@Component({
+    templateUrl: './app/products/product-edit-info.component.html'
+})
+export class ProductEditInfoComponent implements OnInit {
+    @ViewChild(NgForm) productForm: NgForm;
+
+    errorMessage: string;
+    //product = { id: 1, productName: 'test', productCode: 'test' };
+    product : IProduct;
+
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+        this.route.parent.data.subscribe(data =>{
+            this.product = data['product'];
+
+            //Corrige les erreurs du formulaire
+            if (this.productForm){
+                this.productForm.reset();
+            }
+        });
+    }
+}
